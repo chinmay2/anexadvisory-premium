951105771e896c5deffc082b740b2e7fafa8ba36
chinmay2 <69399317+chinmay2@users.noreply.github.com>
2026-09-01 14:31:22 +0530

    fix: allow hero background image from Unsplash

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "framerusercontent.com" },
       { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
